e7a8109faa540d585428a11e2ff8bb3d0db233c0
manoj <manojupadhya11@gmail.com>
2024-03-31 21:36:54 +0530

    Created Modules using modules launched ec2 instance

---
+++ Day 03/modules/ec2_instance/main.tf
@@ -0,0 +1,12 @@
+#Provider block code
+provider "aws" {
+    region = "ap-south-1"
+}
+
+#Creating resource
+resource "aws_instance" "module_example_instance" {
+    ami = var.ami_value 
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id_value
+    associate_public_ip_address = true
+}
