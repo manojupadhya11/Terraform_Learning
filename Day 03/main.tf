bc1750d6eb6a003fab528c5f9fb0f381b8756b8f
manoj <manojupadhya11@gmail.com>
2024-04-04 09:46:09 +0530

    missed fiule

---
+++ Day 03/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+    source  = "./modules/ec2_instance"
+    ami_value =  "ami-007020fd9c84e18c7"
+    instance_type_value =  "t2.micro"
+    subnet_id_value = "subnet-0703716fa2e56fa7c"
+}
