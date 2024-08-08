3258d1ae180ae608258cc1da09ebda21de8d6d9a
manojupadhya11 <manojupadhya11@gmail.com>
2024-08-08 10:56:18 +0530

    added comment in provider.tf file

--- Terraform Learning Projects/Creating Github Repository using HCL/provider.tf
+++ Terraform Learning Projects/Creating Github Repository using HCL/provider.tf
@@ -1,3 +1,3 @@
 provider "github" {
-   token = "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"
+   token = "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"  #Use the access token created in your github profile
 }
